3eec5efc3a8c6f4c38537db9a50c67e3f4976da5
NaMohammed <NaMohammed@INHYDLNaMohammed.logisticsdev.intra>
2019-05-10 16:45:14 +0530

    Call TC

---
+++ Object Repository/Email_id.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Email_id</name>
+   <tag></tag>
+   <elementGuidId>3ce3558e-3e2e-435c-af74-1062bf897576</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;identifierId&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;identifierId&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
